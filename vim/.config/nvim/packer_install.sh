342d9f8e9f2330cd86ef6933ffbc0c48f0ea75cd
turquoise-hexagon <camille@lzr.pw>
2021-09-19 06:32:29 +0100

    rename functions within packer_install.sh to be more similar to plugins.lua

--- vim/.config/nvim/packer_install.sh
+++ vim/.config/nvim/packer_install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-get_packer() {
+packer_install() {
     git clone         \
         "$PACKER_GIT" \
         "$PACKER_DIR" \
         --depth 1
 }
 
-update_packer() {
+packer_init() {
     nvim --headless \
         +'autocmd User PackerComplete qa' +'PackerInstall'
 }
@@ -18,7 +18,7 @@ readonly PACKER_DIR=$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 [ -d "$PACKER_DIR" ] && exit
 
 {
-    get_packer && update_packer
+    packer_install && packer_init
 } > /dev/null 2>&1 || {
     echo "error : failed to run '${0##*/}'" >&2
     exit 1
